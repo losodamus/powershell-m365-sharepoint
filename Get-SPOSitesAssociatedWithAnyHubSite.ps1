20a9693465f89d29e40a7772cd60c262bfaedabd
Charles II <cjlakes@yahoo.com>
2022-09-16 07:06:09 -0700

    Add file via upload

---
+++ Get-SPOSitesAssociatedWithAnyHubSite.ps1
@@ -0,0 +1,40 @@
+<#
+    Get-SPOSitesAssociatedWithAnyHubSite.ps1
+    ------------------------------
+#>
+cls
+
+
+##  Variable(s)
+[System.String] $tenant = "<tenant_name>"
+[System.String] $Login = "<email_address>"
+[System.Security.SecureString] $PWord = ConvertTo-SecureString -String "<password>" -AsPlainText -Force
+
+
+try {
+    Connect-SPOService `
+        -Url "https://$($tenant)-admin.sharepoint.com" `
+        -Credential (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $Login, $PWord) `
+        -ErrorAction Stop
+
+
+    ##  Ignore OneDrive
+    foreach($site in (Get-SPOSite -Limit All -IncludePersonalSite $false `
+        | ? { $_.HubSiteId -eq "00000000-0000-0000-0000-000000000000" } `
+        | Select Title, Url, HubSiteId, StorageUsageCurrent `
+        | Sort-Object Title )) {
+
+
+        Write-Output "$($site.Title)"
+        Write-Output "$($site.Url)`n"
+    }
+}
+catch [Exception] {
+    Write-Error "$($_.Exception.Message)"
+}
+finally {
+    try {
+        Disconnect-SPOService
+    }
+    catch { }
+}
